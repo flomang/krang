d9ecbdb9d7e29dc9c0af7cac23310e6d11d4fbaa
flo <inbox.733@gmail.com>
2022-07-22 21:17:55 -0600

    Added additional structs for genetic algo lib.

--- libs/genetic-algorithm/src/lib.rs
+++ libs/genetic-algorithm/src/lib.rs
@@ -1,3 +1,14 @@
+use rand::seq::SliceRandom;
+use rand::RngCore;
+
+pub struct RouletteWheelSelection;
+
+impl RouletteWheelSelection {
+    pub fn new() -> Self {
+        Self
+    }
+}
+
 pub struct GeneticAlgorithm;
 
 impl GeneticAlgorithm {
@@ -6,15 +17,79 @@ impl GeneticAlgorithm {
     }
 
     pub fn evolve<I>(&self, population: &[I]) -> Vec<I> {
+        assert!(!population.is_empty());
+        //(0..population.len())
+        //    .map(|_| {
+        //        // TODO selection
+        //        // TODO crossover
+        //        // TODO mutation
+        //        todo!()
+        //    })
+        //    .collect();
         todo!()
+    }
+}
+
+pub trait Individual {
+    fn fitness(&self) -> f32;
+}
+
+pub trait SelectionMethod {
+    fn select<'a, I>(&self, rng: &mut dyn RngCore, population: &'a [I]) -> &'a I
+    where
+        I: Individual;
+}
+
+impl SelectionMethod for RouletteWheelSelection {
+    fn select<'a, I>(&self, rng: &mut dyn RngCore, population: &'a [I]) -> &'a I
+    where
+        I: Individual,
+    {
+        population
+            .choose_weighted(rng, |individual| individual.fitness())
+            .expect("got an empty population")
     }
 }
 
 #[cfg(test)]
 mod tests {
+    use super::*;
+    use rand::SeedableRng;
+    use rand_chacha::ChaCha8Rng;
+
+    #[cfg(test)]
+    #[derive(Clone, Debug)]
+    pub struct TestIndividual {
+        fitness: f32,
+    }
+
+    #[cfg(test)]
+    impl TestIndividual {
+        pub fn new(fitness: f32) -> Self {
+            Self { fitness }
+        }
+    }
+
+    #[cfg(test)]
+    impl Individual for TestIndividual {
+        fn fitness(&self) -> f32 {
+            self.fitness
+        }
+    }
+
     #[test]
-    fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
+    fn test() {
+        let mut rng = ChaCha8Rng::from_seed(Default::default());
+
+        let population = vec![
+            TestIndividual::new(2.0),
+            TestIndividual::new(1.0),
+            TestIndividual::new(4.0),
+            TestIndividual::new(3.0),
+        ];
+
+        let actual = RouletteWheelSelection::new().select(&mut rng, &population);
+
+        assert!(true);
     }
 }
