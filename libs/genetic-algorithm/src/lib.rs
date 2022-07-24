6b6a9b8e78706ddb237b61ad183eeda65390cc8e
flo <inbox.733@gmail.com>
2022-07-23 19:08:45 -0600

    Passing roulette wheel selection test.

--- libs/genetic-algorithm/src/lib.rs
+++ libs/genetic-algorithm/src/lib.rs
@@ -69,6 +69,8 @@ mod tests {
     use super::*;
     use rand::SeedableRng;
     use rand_chacha::ChaCha8Rng;
+    use std::collections::BTreeMap;
+
 
     #[cfg(test)]
     #[derive(Clone, Debug)]
@@ -92,6 +94,7 @@ mod tests {
 
     #[test]
     fn test() {
+        let method = RouletteWheelSelection::new();
         let mut rng = ChaCha8Rng::from_seed(Default::default());
 
         let population = vec![
@@ -101,8 +104,28 @@ mod tests {
             TestIndividual::new(3.0),
         ];
 
-        let _actual = RouletteWheelSelection::new().select(&mut rng, &population);
+        let mut actual_histogram = BTreeMap::new();
 
-        assert!(true);
+        //               there is nothing special about this thousand;
+        //          v--v a number as low as fifty might do the trick, too
+        for _ in 0..1000 {
+            let fitness = method
+                .select(&mut rng, &population)
+                .fitness() as i32;
+    
+            *actual_histogram
+                .entry(fitness)
+                .or_insert(0) += 1;
+        }
+    
+        let expected_histogram = BTreeMap::from_iter(vec![
+            // (fitness, how many times this fitness has been chosen)
+            (1, 98),
+            (2, 202),
+            (3, 278),
+            (4, 422),
+        ]);
+    
+        assert_eq!(actual_histogram, expected_histogram);
     }
 }
