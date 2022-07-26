0782cbbc410e43e80e8057d47d1bc28d0e955e3f
flo <inbox.733@gmail.com>
2022-07-25 19:01:07 -0600

    Implemented mutation and evolve with tests.

--- libs/genetic-algorithm/src/lib.rs
+++ libs/genetic-algorithm/src/lib.rs
@@ -13,7 +13,12 @@ pub trait CrossoverMethod {
     ) -> Chromosome;
 }
 
+pub trait MutationMethod {
+    fn mutate(&self, rng: &mut dyn RngCore, child: &mut Chromosome);
+}
+
 pub trait Individual {
+    fn create(chromosome: Chromosome) -> Self;
     fn chromosome(&self) -> &Chromosome;
     fn fitness(&self) -> f32;
 }
@@ -92,16 +97,22 @@ impl SelectionMethod for RouletteWheelSelection {
 pub struct GeneticAlgorithm<S> {
     selection_method: S,
     crossover_method: Box<dyn CrossoverMethod>,
+    mutation_method: Box<dyn MutationMethod>,
 }
 
 impl<S> GeneticAlgorithm<S>
 where
     S: SelectionMethod,
 {
-    pub fn new(selection_method: S, crossover_method: impl CrossoverMethod + 'static) -> Self {
+    pub fn new(
+        selection_method: S,
+        crossover_method: impl CrossoverMethod + 'static,
+        mutation_method: impl MutationMethod + 'static,
+    ) -> Self {
         Self {
             selection_method,
             crossover_method: Box::new(crossover_method),
+            mutation_method: Box::new(mutation_method),
         }
     }
 
@@ -117,10 +128,11 @@ where
 
                 let parent_b = self.selection_method.select(rng, population).chromosome();
 
-                let mut _child = self.crossover_method.crossover(rng, parent_a, parent_b);
+                let mut child = self.crossover_method.crossover(rng, parent_a, parent_b);
 
-                // TODO mutation
-                todo!()
+                self.mutation_method.mutate(rng, &mut child);
+
+                I::create(child)
             })
             .collect()
     }
@@ -154,6 +166,39 @@ impl CrossoverMethod for UniformCrossover {
     }
 }
 
+#[derive(Clone, Debug)]
+pub struct GaussianMutation {
+    /// Probability of changing a gene:
+    /// - 0.0 = no genes will be touched
+    /// - 1.0 = all genes will be touched
+    chance: f32,
+
+    /// Magnitude of that change:
+    /// - 0.0 = touched genes will not be modified
+    /// - 3.0 = touched genes will be += or -= by at most 3.0
+    coeff: f32,
+}
+
+impl GaussianMutation {
+    pub fn new(chance: f32, coeff: f32) -> Self {
+        assert!(chance >= 0.0 && chance <= 1.0);
+
+        Self { chance, coeff }
+    }
+}
+
+impl MutationMethod for GaussianMutation {
+    fn mutate(&self, rng: &mut dyn RngCore, child: &mut Chromosome) {
+        for gene in child.iter_mut() {
+            let sign = if rng.gen_bool(0.5) { -1.0 } else { 1.0 };
+
+            if rng.gen_bool(self.chance as _) {
+                *gene += sign * self.coeff * rng.gen::<f32>();
+            }
+        }
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -161,27 +206,94 @@ mod tests {
     use rand_chacha::ChaCha8Rng;
     use std::collections::BTreeMap;
 
+    fn actual(chance: f32, coeff: f32) -> Vec<f32> {
+        let mut child = vec![1.0, 2.0, 3.0, 4.0, 5.0].into_iter().collect();
+
+        let mut rng = ChaCha8Rng::from_seed(Default::default());
+
+        GaussianMutation::new(chance, coeff).mutate(&mut rng, &mut child);
+
+        child.into_iter().collect()
+    }
+
+    //#[cfg(test)]
+    //#[derive(Clone, Debug)]
+    //pub struct TestIndividual {
+    //    fitness: f32,
+    //}
+
+    // #[cfg(test)]
+    // impl TestIndividual {
+    //     pub fn new(fitness: f32) -> Self {
+    //         Self { fitness }
+    //     }
+    // }
+
+    // #[cfg(test)]
+    // impl Individual for TestIndividual {
+    //     fn fitness(&self) -> f32 {
+    //         self.fitness
+    //     }
+
+    //     fn chromosome(&self) -> &Chromosome {
+    //         panic!("not supported for TestIndividual")
+    //     }
+
+    //     fn create(chromosome: Chromosome) -> Self {
+    //         todo!()
+    //     }
+    // }
     #[cfg(test)]
-    #[derive(Clone, Debug)]
-    pub struct TestIndividual {
-        fitness: f32,
+    #[derive(Clone, Debug, PartialEq)]
+    pub enum TestIndividual {
+        /// For tests that require access to chromosome
+        WithChromosome { chromosome: Chromosome },
+
+        /// For tests that don't require access to chromosome
+        WithFitness { fitness: f32 },
+    }
+
+    #[cfg(test)]
+    impl PartialEq for Chromosome {
+        fn eq(&self, other: &Self) -> bool {
+            approx::relative_eq!(self.genes.as_slice(), other.genes.as_slice(),)
+        }
     }
 
     #[cfg(test)]
     impl TestIndividual {
         pub fn new(fitness: f32) -> Self {
-            Self { fitness }
+            Self::WithFitness { fitness }
         }
     }
 
     #[cfg(test)]
     impl Individual for TestIndividual {
-        fn fitness(&self) -> f32 {
-            self.fitness
+        fn create(chromosome: Chromosome) -> Self {
+            Self::WithChromosome { chromosome }
         }
 
         fn chromosome(&self) -> &Chromosome {
-            panic!("not supported for TestIndividual")
+            match self {
+                Self::WithChromosome { chromosome } => chromosome,
+
+                Self::WithFitness { .. } => {
+                    panic!("not supported for TestIndividual::WithFitness")
+                }
+            }
+        }
+
+        fn fitness(&self) -> f32 {
+            match self {
+                Self::WithChromosome { chromosome } => {
+                    chromosome.iter().sum()
+
+                    // ^ the simplest fitness function ever - we're just
+                    // summing all the genes together
+                }
+
+                Self::WithFitness { fitness } => *fitness,
+            }
         }
     }
 
@@ -336,6 +448,150 @@ mod tests {
 
             assert_eq!(diff_a, 49);
             assert_eq!(diff_b, 51);
+        }
+    }
+
+    mod given_zero_chance {
+        fn actual(coeff: f32) -> Vec<f32> {
+            super::actual(0.0, coeff)
+        }
+
+        mod and_zero_coefficient {
+            use super::*;
+
+            #[test]
+            fn does_not_change_the_original_chromosome() {
+                let actual = actual(0.0);
+                let expected = vec![1.0, 2.0, 3.0, 4.0, 5.0];
+
+                approx::assert_relative_eq!(actual.as_slice(), expected.as_slice(),);
+            }
+        }
+
+        mod and_nonzero_coefficient {
+            use super::*;
+
+            #[test]
+            fn does_not_change_the_original_chromosome() {
+                let actual = actual(0.5);
+                let expected = vec![1.0, 2.0, 3.0, 4.0, 5.0];
+
+                approx::assert_relative_eq!(actual.as_slice(), expected.as_slice(),);
+            }
+        }
+    }
+
+    mod given_fifty_fifty_chance {
+        fn actual(coeff: f32) -> Vec<f32> {
+            super::actual(0.5, coeff)
+        }
+
+        mod and_zero_coefficient {
+            use super::*;
+
+            #[test]
+            fn does_not_change_the_original_chromosome() {
+                let actual = actual(0.0);
+                let expected = vec![1.0, 2.0, 3.0, 4.0, 5.0];
+
+                approx::assert_relative_eq!(actual.as_slice(), expected.as_slice(),);
+            }
+        }
+
+        mod and_nonzero_coefficient {
+            use super::*;
+
+            #[test]
+            fn slightly_changes_the_original_chromosome() {
+                let actual = actual(0.5);
+                let expected = vec![1.0, 1.7756249, 3.0, 4.1596804, 5.0];
+
+                approx::assert_relative_eq!(actual.as_slice(), expected.as_slice(),);
+            }
+        }
+    }
+
+    mod given_max_chance {
+        fn actual(coeff: f32) -> Vec<f32> {
+            super::actual(1.0, coeff)
+        }
+
+        mod and_zero_coefficient {
+            use super::*;
+
+            #[test]
+            fn does_not_change_the_original_chromosome() {
+                let actual = actual(0.0);
+                let expected = vec![1.0, 2.0, 3.0, 4.0, 5.0];
+
+                approx::assert_relative_eq!(actual.as_slice(), expected.as_slice(),);
+            }
+        }
+
+        mod and_nonzero_coefficient {
+            use super::*;
+
+            #[test]
+            fn entirely_changes_the_original_chromosome() {
+                let actual = actual(0.5);
+
+                let expected = vec![1.4545316, 2.1162078, 2.7756248, 3.9505124, 4.638691];
+
+                approx::assert_relative_eq!(actual.as_slice(), expected.as_slice(),);
+            }
+        }
+    }
+
+    #[cfg(test)]
+    mod genetic_algo {
+        use super::*;
+        use rand::SeedableRng;
+        use rand_chacha::ChaCha8Rng;
+
+        fn individual(genes: &[f32]) -> TestIndividual {
+            let chromosome = genes.iter().cloned().collect();
+
+            TestIndividual::create(chromosome)
+        }
+
+        #[test]
+        fn test() {
+            let mut rng = ChaCha8Rng::from_seed(Default::default());
+
+            let ga = GeneticAlgorithm::new(
+                RouletteWheelSelection::new(),
+                UniformCrossover::new(),
+                GaussianMutation::new(0.5, 0.5),
+            );
+
+            let mut population = vec![
+                individual(&[0.0, 0.0, 0.0]), // fitness = 0.0
+                individual(&[1.0, 1.0, 1.0]), // fitness = 3.0
+                individual(&[1.0, 2.0, 1.0]), // fitness = 4.0
+                individual(&[1.0, 2.0, 4.0]), // fitness = 7.0
+            ];
+
+            // We're running `.evolve()` a few times, so that the
+            // differences between initial and output population are
+            // easier to spot.
+            //
+            // No particular reason for a number of 10 - this test would
+            // be fine for 5, 20 or even 1000 generations; the only thing
+            // that'd change is the *magnitude* of difference between
+            // initial and output population.
+            for _ in 0..10 {
+                population = ga.evolve(&mut rng, &population);
+            }
+
+
+            let expected_population = vec![
+                individual(&[0.44769490, 2.0648358, 4.3058133]),
+                individual(&[1.21268670, 1.5538777, 2.8869110]),
+                individual(&[1.06176780, 2.2657390, 4.4287640]),
+                individual(&[0.95909685, 2.4618788, 4.0247330]),
+            ];
+
+            assert_eq!(population, expected_population);
         }
     }
 }
