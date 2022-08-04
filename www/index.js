17bac336b28aefc77a4246a0f8677fff706f4b5f
flo <inbox.733@gmail.com>
2022-08-03 19:10:09 -0600

    Added world to simulation.

--- www/index.js
+++ www/index.js
@@ -1,3 +1,7 @@
 import * as sim from "lib-simulation-wasm";
 
+const simulation = new sim.Simulation();
+const world = simulation.world();
+console.log(world);
+
 alert("Who's that dog? " + sim.whos_that_dog() + "!");
