1262efe9f17a2b44ba7c64a7cd143409d38be568
flo <inbox.733@gmail.com>
2022-10-15 22:00:05 -0600

    Semi intelligent birds.

--- libs/simulation/src/lib.rs
+++ libs/simulation/src/lib.rs
@@ -1,12 +1,52 @@
-pub use self::{animal::*, food::*, world::*};
+pub use self::{animal::*, eye::*, food::*, world::*};
 
 mod animal;
+mod eye;
 mod food;
 mod world;
 
+use lib_neural_network as nn;
 use nalgebra as na;
 use rand::{Rng, RngCore};
 
+// FRAC_PI_2 = PI / 2.0; a convenient shortcut
+use std::f32::consts::FRAC_PI_2;
+
+/// Minimum speed of a bird.
+///
+/// Keeping it above zero prevents birds from getting stuck in one place.
+const SPEED_MIN: f32 = 0.001;
+
+/// Maximum speed of a bird.
+///
+/// Keeping it "sane" prevents birds from accelerating up to infinity,
+/// which makes the simulation... unrealistic :-)
+const SPEED_MAX: f32 = 0.005;
+
+/// Speed acceleration; determines how much the brain can affect bird's
+/// speed during one step.
+///
+/// Assuming our bird is currently flying with speed=0.5, when the brain
+/// yells "stop flying!", a SPEED_ACCEL of:
+///
+/// - 0.1 = makes it take 5 steps ("5 seconds") for the bird to actually
+///         slow down to SPEED_MIN,
+///
+/// - 0.5 = makes it take 1 step for the bird to slow down to SPEED_MIN.
+///
+/// This improves simulation faithfulness, because - as in real life -
+/// it's not possible to increase speed from 1km/h to 50km/h in one
+/// instant, even if your brain very much wants to.
+const SPEED_ACCEL: f32 = 0.2;
+
+/// Ditto, but for rotation:
+///
+/// - 2 * PI = it takes one step for the bird to do a 360° rotation,
+/// - PI = it takes two steps for the bird to do a 360° rotation,
+///
+/// I've chosen PI/2, because - as our motto goes - this value seems
+/// to play nice.
+const ROTATION_ACCEL: f32 = FRAC_PI_2;
 
 pub struct Simulation {
     world: World,
@@ -23,6 +63,7 @@ impl Simulation {
     /// simulation.
     pub fn step(&mut self, rng: &mut dyn RngCore) {
         self.process_collisions(rng);
+        self.process_brains();
         self.process_movements();
     }
 
@@ -30,14 +71,50 @@ impl Simulation {
         &self.world
     }
 
+    fn process_brains(&mut self) {
+        for animal in &mut self.world.animals {
+            let vision =
+                animal
+                    .eye
+                    .process_vision(animal.position, animal.rotation, &self.world.foods);
+
+            let response = animal.brain.propagate(vision);
+
+            // ---
+            // | Limits number to given range.
+            // -------------------- v---v
+            let speed = response[0].clamp(-SPEED_ACCEL, SPEED_ACCEL);
+
+            let rotation = response[1].clamp(-ROTATION_ACCEL, ROTATION_ACCEL);
+
+            // Our speed & rotation here are *relative* - that is: when
+            // they are equal to zero, what the brain says is "keep
+            // flying as you are now", not "stop flying".
+            //
+            // Both values being relative is crucial, because our bird's
+            // brain doesn't know its own speed and rotation*, meaning
+            // that it fundamentally cannot return absolute values.
+            //
+            // * they'd have to be provided as separate inputs to the
+            //   neural network, which would make the evolution process
+            //   waaay longer, if even possible.
+
+            animal.speed = (animal.speed + speed).clamp(SPEED_MIN, SPEED_MAX);
+
+            animal.rotation = na::Rotation2::new(animal.rotation.angle() + rotation);
+
+            // (btw, there is no need for ROTATION_MIN or ROTATION_MAX,
+            // because rotation automatically wraps from 2*PI back to 0 -
+            // we've already witnessed that when we were testing eyes,
+            // inside `mod different_rotations { ... }`.)
+        }
+    }
+
     fn process_collisions(&mut self, rng: &mut dyn RngCore) {
         for animal in &mut self.world.animals {
             for food in &mut self.world.foods {
-                let distance = na::distance(
-                    &animal.position,
-                    &food.position,
-                );
-    
+                let distance = na::distance(&animal.position, &food.position);
+
                 if distance <= 0.01 {
                     food.position = rng.gen();
                 }
@@ -47,15 +124,13 @@ impl Simulation {
 
     fn process_movements(&mut self) {
         for animal in &mut self.world.animals {
-            animal.position +=
-                animal.rotation * na::Vector2::new(animal.speed, 0.0);
+            animal.position += animal.rotation * na::Vector2::new(animal.speed, 0.0);
 
             animal.position.x = na::wrap(animal.position.x, 0.0, 1.0);
             animal.position.y = na::wrap(animal.position.y, 0.0, 1.0);
         }
     }
 }
-
 
 #[derive(Debug)]
 pub struct Point2 {
